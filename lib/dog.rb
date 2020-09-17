bf521a2f3ca2208ba2400e77ca6f143b1e8cae36
Jc Dior <jessicaclairdior@gmail.com>
2020-09-17 20:11:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Add your code here
+class Dog
+
+@@all = []
+
+def initialize
+  save
+end
+
+def self.all
+@@all.all
+end
+
+def self.print_all
+  @@all.each do |puppy|
+    puts puppy
+  end
+end
+
+def self.save
+  @@all << self
+end
+  
+end
